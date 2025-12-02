fa2565510c68375f69fb39e205d5bf8e7e1e44dc
moonyango <mosesadrian825@gmail.com>
2025-12-02 17:08:16 +0300

    feat: complete implementation of Day 1 part 2 logic

--- 2025/src/days/day_01.rs
+++ 2025/src/days/day_01.rs
@@ -34,5 +34,39 @@ fn part1() {
 }
 
 fn part2() {
-    println!("Running Day 1 Part 2");
+    let mut position: i32 = 50;
+    let mut count = 0;
+    let lines = parse::load_lines("src/inputs/day_01.txt");
+
+    for line in lines {
+        if line.is_empty() {
+            continue;
+        }
+        let direction = &line[0..1];
+        let val: i32 = line[1..].parse().unwrap();
+        let delta = match direction {
+            "R" => val,
+            "L" => -val,
+            _ => panic!("Invalid direction"),
+        };
+        let new_position = (position + delta).rem_euclid(100);
+
+        // Count how many times we passed through 0
+        let mut passes = (position + delta).div_euclid(100);
+        // If the final position is exactly 0, it counts as a pass
+        if position + delta == 0 {
+            passes += 1;
+        }
+        if (position + delta) < 0 && position == 0 {
+            passes += 1;
+        }
+        if (new_position == 0) && (position + delta) < 0 {
+            passes -= 1;
+        }
+
+        count += passes.abs();
+
+        position = new_position;
+    }
+    println!("Solution to day 1 part 2 {}", count)
 }
