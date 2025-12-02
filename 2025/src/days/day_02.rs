ea6554e9fd0684f0f755f11619fe7579115f966d
moonyango <mosesadrian825@gmail.com>
2025-12-02 14:20:50 +0300

    feat: implement Day 2 execution logic with part 1 and part 2 functions

---
+++ 2025/src/days/day_02.rs
@@ -0,0 +1,15 @@
+pub fn run(part: u8) {
+    match part {
+        1 => part1(),
+        2 => part2(),
+        _ => println!("Invalid part"),
+    }
+}
+
+fn part1() {
+    println!("Running Day 2 Part 1");
+}
+
+fn part2() {
+    println!("Running Day 2 Part 2");
+}
