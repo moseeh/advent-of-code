db1d1870fd48cf8bb5457f049a02e6259c4063d1
moonyango <mosesadrian825@gmail.com>
2025-12-02 14:20:59 +0300

    feat: implement main function to parse CLI arguments and execute specified day and part

--- 2025/src/main.rs
+++ 2025/src/main.rs
@@ -1,3 +1,13 @@
+mod cli;
+mod days;
+mod utils;
+
+use clap::Parser;
+use cli::Cli;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("Running day {}, part {}", args.day, args.part);
+
+    days::run(args.day, args.part);
 }
