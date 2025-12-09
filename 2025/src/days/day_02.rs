fe34a1924caf7b41df43a1372c24ede1e690fa48
moonyango <mosesadrian825@gmail.com>
2025-12-09 18:02:08 +0300

    fix: clean up formatting and improve readability in Day 2 implementation

--- 2025/src/days/day_02.rs
+++ 2025/src/days/day_02.rs
@@ -10,7 +10,7 @@ pub fn run(part: u8) {
 
 fn part1() {
     let input = parse::load_file("src/inputs/day_02.txt");
-    
+
     let mut sum: u64 = 0;
     for range in input.split(",") {
         let mut it = range.split("-");
@@ -23,20 +23,51 @@ fn part1() {
         }
     }
     println!("{}", sum)
-
 }
 
 fn is_invalid_id(n: u64) -> bool {
     let s = n.to_string();
     let len = s.len();
-    if len %2 != 0 {
+    if len % 2 != 0 {
         return false;
     }
     let half = len / 2;
     &s[..half] == &s[half..]
-
 }
 
 fn part2() {
-    println!("Running Day 2 Part 2");
+    let input = parse::load_file("src/inputs/day_02.txt");
+    let mut sum: u64 = 0;
+    for range in input.split(",") {
+        let mut it = range.split("-");
+        let start: u64 = it.next().unwrap().parse().unwrap();
+        let end: u64 = it.next().unwrap().parse().unwrap();
+        for id in start..=end {
+            if is_invalid_id_2(id) {
+                sum += id;
+            }
+        }
+    }
+    println!("{}", sum)
+}
+
+fn is_invalid_id_2(n: u64) -> bool {
+    let s = n.to_string();
+    let len = s.len();
+
+    // Pattern must repeat at least twice
+    for pat_len in 1..=len / 2 {
+        if len % pat_len != 0 {
+            continue;
+        }
+
+        let pattern = &s[..pat_len];
+        let repeats = len / pat_len;
+
+        if pattern.repeat(repeats) == s {
+            return true;
+        }
+    }
+
+    false
 }
